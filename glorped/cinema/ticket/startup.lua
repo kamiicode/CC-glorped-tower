8b3af52da135d7f6f4d8a5f491257849dacb67bb
kamiicode <77517394+kamiicode@users.noreply.github.com>
2026-05-17 21:05:29 -0300

    BUCETA

--- glorped/cinema/ticket/startup.lua
+++ glorped/cinema/ticket/startup.lua
@@ -2,11 +2,11 @@ local input_inventory = peripheral.find("sophisticatedstorage:barrel")
 local output_inventory = peripheral.find("minecraft:chest")
 
 while true do
-  for items in pairs(input_inventory.list()) do
-    if barrel.getItemDetail(i).name == "minecraft:slime" then
+  for slot, item in pairs(input_inventory.list()) do
+    if barrel.getItemDetail(slot).name == "minecraft:slime" then
         input_inventory.pushItems(output_inventory.name, 1)
     else
-        print(i)
+        print(slot, item)
     end
   end
   sleep(1)
